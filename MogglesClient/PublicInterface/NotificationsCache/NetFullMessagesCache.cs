ce423cd638e6e39ab0a00a533d40935335510a3a
andrei.lengyel@evozon.onmicrosoft.com <andrei.lengyel@evozon.onmicrosoft.com>
2021-05-11 14:01:40 +0300

    added cooldowns to stop notification spamming

---
+++ MogglesClient/PublicInterface/NotificationsCache/NetFullMessagesCache.cs
@@ -0,0 +1,28 @@
+﻿#if NETFULL
+using MogglesClient.PublicInterface.Notifications;
+using System;
+using System.Runtime.Caching;
+
+namespace MogglesClient.PublicInterface.NotificationsCache
+{
+    public class NetFullNotificationsCache: INotificationsCache
+    {
+        private MemoryCache Cache { get; set; }
+
+        public NetFullNotificationsCache()
+        {
+            Cache = new MemoryCache("NotificationsCache");
+        }
+
+        public void CacheNotification(Message message, DateTimeOffset absoluteExpiration)
+        {   
+            Cache.Add(message.text, message, absoluteExpiration);
+        }
+
+        public bool NotificationExists(Message message)
+        {
+            return Cache.Contains(message.text);
+        }
+    }
+}
+#endif
